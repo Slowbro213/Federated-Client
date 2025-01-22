9506a829a5bd516f3983323a71072cb2a68cc745
ThanasPapa <100229861+Slowbro213@users.noreply.github.com>
2025-01-22 22:43:25 +0100

    flower-new

--- client.py
+++ client.py
@@ -1,22 +1,6 @@
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
+import tensorflow as tf
 import flwr as fl
-
-# Define a simple neural network
-class Net(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_size, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, output_size)
-
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        return self.fc3(x)
 
 # Load dataset for this client
 client_id = 0  # Change this for each VM (0, 1, 2, ...)
@@ -24,65 +8,37 @@ data = np.load(f"client_{client_id}.npz")
 X_train, y_train = data["X_train"], data["y_train"]
 X_test, y_test = data["X_test"], data["y_test"]
 
-# Convert to PyTorch tensors
-X_train = torch.tensor(X_train, dtype=torch.float32)
-y_train = torch.tensor(y_train, dtype=torch.long)
-X_test = torch.tensor(X_test, dtype=torch.float32)
-y_test = torch.tensor(y_test, dtype=torch.long)
+# Convert to TensorFlow datasets
+train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).batch(32)
+test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(32)
 
-# Create DataLoader
-train_dataset = TensorDataset(X_train, y_train)
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_dataset = TensorDataset(X_test, y_test)
-test_loader = DataLoader(test_dataset, batch_size=32)
+# Define a simple neural network
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation="relu", input_shape=(X_train.shape[1],)),
+    tf.keras.layers.Dense(32, activation="relu"),
+    tf.keras.layers.Dense(len(np.unique(y_train)), activation="softmax"),
+])
+
+# Compile the model
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
 # Define Flower client
 class NetflixClient(fl.client.NumPyClient):
-    def __init__(self, model, train_loader, test_loader):
-        self.model = model
-        self.train_loader = train_loader
-        self.test_loader = test_loader
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model.to(self.device)
-
     def get_parameters(self, config):
-        return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
+        return model.get_weights()
 
     def set_parameters(self, parameters):
-        params_dict = zip(self.model.state_dict().keys(), parameters)
-        state_dict = {k: torch.tensor(v) for k, v in params_dict}
-        self.model.load_state_dict(state_dict)
+        model.set_weights(parameters)
 
     def fit(self, parameters, config):
         self.set_parameters(parameters)
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(self.model.parameters(), lr=0.01)
-        self.model.train()
-        for epoch in range(5):  # Train for 5 epochs
-            for inputs, labels in self.train_loader:
-                inputs, labels = inputs.to(self.device), labels.to(self.device)
-                optimizer.zero_grad()
-                outputs = self.model(inputs)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
-        return self.get_parameters(config), len(self.train_loader.dataset), {}
+        model.fit(train_dataset, epochs=5, verbose=0)  # Train for 5 epochs
+        return self.get_parameters(config), len(X_train), {}
 
     def evaluate(self, parameters, config):
         self.set_parameters(parameters)
-        criterion = nn.CrossEntropyLoss()
-        self.model.eval()
-        loss, correct = 0, 0
-        with torch.no_grad():
-            for inputs, labels in self.test_loader:
-                inputs, labels = inputs.to(self.device), labels.to(self.device)
-                outputs = self.model(inputs)
-                loss += criterion(outputs, labels).item()
-                _, predicted = torch.max(outputs.data, 1)
-                correct += (predicted == labels).sum().item()
-        accuracy = correct / len(self.test_loader.dataset)
-        return loss, len(self.test_loader.dataset), {"accuracy": accuracy}
+        loss, accuracy = model.evaluate(test_dataset, verbose=0)
+        return loss, len(X_test), {"accuracy": accuracy}
 
 # Start Flower client
-model = Net(input_size=X_train.shape[1], output_size=len(np.unique(y_train)))
-fl.client.start_numpy_client(server_address="[SERVER_IP]:8080", client=NetflixClient(model, train_loader, test_loader))
+fl.client.start_numpy_client(server_address="[SERVER_IP]:8080", client=NetflixClient())
